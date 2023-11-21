08b1205bef36c0093a5fc552f139c3d765c4d4ef
Rimpi <rimpisuman@gmail.com>
2023-11-21 17:39:41 +1100

    Logo setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-
-import './App.css';
-import Home from './pages/Home/HomeScreen';
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+import Home from "./pages/Home/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-   
-  <Home/>
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />;
+      </div>
+      <Home />
     </div>
   );
 }
